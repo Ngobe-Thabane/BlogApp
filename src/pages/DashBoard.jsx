486cfdacc23b30d969193abebb29b4930cc89d52
Thabane-Ngobe <tngobe@dvt.co.za>
2025-01-15 13:35:19 +0200

    fixed minor problems

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -5,7 +5,6 @@ export default function DashBoard() {
   return(
     <div className="blogs">
       <div className="blog-intro">
-        <h1>Wlecome to the Project Blog</h1>
         <p>Blog about Frontend, Backend, DevOps and everything in between.</p>
       </div>
       <PostList POSTS={POSTS} />
